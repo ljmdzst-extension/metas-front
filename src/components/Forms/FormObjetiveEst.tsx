241d3fba5db98ec5fa09029749183c1fd8e7faef
HernanC95 <cavalieri.h95@gmail.com>
2023-06-30 12:05:11 -0300

    createFormObj

--- src/components/Forms/FormObjetiveEst.tsx
+++ src/components/Forms/FormObjetiveEst.tsx
@@ -1,7 +1,34 @@
-import React from 'react'
+import React from "react";
+import { Form } from "react-bootstrap";
 
 export default function FormObjetiveEst() {
   return (
-    <div className='FormDescription'>FormObjetiveEst</div>
-  )
+    <>
+      <div className="FormObjetivo">
+        <h2 className="TitleObjetivo">Objetivo Estrategico</h2>
+        <Form className="FormObj">
+          <p className="SubtitleObj">Seleccione los ejes:</p>
+          <div className="Obj">
+            <Form.Check
+              id={`1`}
+              label={`Profundizar la integración de funciones, con especial énfasis en el fortalecimiento de las condiciones que permitan incrementar los espacios y carreras con Prácticas de Extensión de Educación Experiencial.`}
+            />
+            <Form.Check
+              id={`2`}
+              label={`Promover la intervención territorial, las actividades culturales, la preservación y circulación del patrimonio tangible e intangible de la UNL y la región, profundizando la transversalidad entre las Áreas Centrales y las Unidades Académicas.`}
+            />
+            <Form.Check
+              id={`3`}
+              label={`Generar y propiciar actividades de formación y capacitación desde una mirada inclusiva con carácter interdisciplinario favoreciendo la inserción en el mundo del trabajo.`}
+            />
+            <Form.Check
+              id={`4`}
+              label={`Consolidar la jerarquización e internacionalización de la extensión, las actividades culturales y artísticas, mediante estrategias de formación, capacitación y comunicación.`}
+            />
+          </div>
+        </Form>
+      </div>
+      <button className="SaveChange">Guardar Cambios</button>
+    </>
+  );
 }
--- src/components/Forms/FormPIE.tsx
+++ src/components/Forms/FormPIE.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Form } from "react-bootstrap";
+import {Form } from "react-bootstrap";
 export default function FormPIE() {
   return (
     <>
@@ -21,23 +21,23 @@ export default function FormPIE() {
             <p className="SubtitlePie">Seleccione los planes:</p>
             <div className="ConteinerChecksPlan">
               <div className="Lie">
-                <Form.Check id={`1`} label={`L.I.E N°1 - Objetivo 1`} />
-                <Form.Check id={`2`} label={`L.I.E N°1 - Objetivo 2`} />
-                <Form.Check id={`3`} label={`L.I.E N°1 - Objetivo 3`} />
-                <Form.Check id={`3`} label={`L.I.E N°1 - Objetivo 4`} />
+                <Form.Check id={`4`} label={`L.I.E N°1 - Objetivo 1`} />
+                <Form.Check id={`5`} label={`L.I.E N°1 - Objetivo 2`} />
+                <Form.Check id={`6`} label={`L.I.E N°1 - Objetivo 3`} />
+                <Form.Check id={`7`} label={`L.I.E N°1 - Objetivo 4`} />
               </div>
               <div className="Lie">
-                <Form.Check id={`1`} label={`L.I.E N°2 - Objetivo 1`} />
-                <Form.Check id={`2`} label={`L.I.E N°2 - Objetivo 2`} />
-                <Form.Check id={`3`} label={`L.I.E N°2 - Objetivo 3`} />
-                <Form.Check id={`3`} label={`L.I.E N°2 - Objetivo 4`} />
+                <Form.Check id={`8`} label={`L.I.E N°2 - Objetivo 1`} />
+                <Form.Check id={`9`} label={`L.I.E N°2 - Objetivo 2`} />
+                <Form.Check id={`10`} label={`L.I.E N°2 - Objetivo 3`} />
+                <Form.Check id={`11`} label={`L.I.E N°2 - Objetivo 4`} />
               </div>
               <div className="Lie">
-                <Form.Check id={`1`} label={`L.I.E N°3 - Objetivo 1`} />
-                <Form.Check id={`2`} label={`L.I.E N°3 - Objetivo 2`} />
-                <Form.Check id={`3`} label={`L.I.E N°3 - Objetivo 3`} />
-                <Form.Check id={`3`} label={`L.I.E N°3 - Objetivo 4`} />
-                <Form.Check id={`3`} label={`L.I.E N°3 - Objetivo 5`} />
+                <Form.Check id={`12`} label={`L.I.E N°3 - Objetivo 1`} />
+                <Form.Check id={`13`} label={`L.I.E N°3 - Objetivo 2`} />
+                <Form.Check id={`14`} label={`L.I.E N°3 - Objetivo 3`} />
+                <Form.Check id={`15`} label={`L.I.E N°3 - Objetivo 4`} />
+                <Form.Check id={`16`} label={`L.I.E N°3 - Objetivo 5`} />
               </div>
             </div>
           </Form>
