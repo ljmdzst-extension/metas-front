e5baf7ad7c0e85385a47ba06104b9bc137d96b7a
HernanC95 <cavalieri.h95@gmail.com>
2023-06-30 11:24:12 -0300

    createFormPeriodo&PIE

--- src/components/Forms/FormPIE.tsx
+++ src/components/Forms/FormPIE.tsx
@@ -1,7 +1,49 @@
-import React from 'react'
-
+import React from "react";
+import { Container, Form } from "react-bootstrap";
 export default function FormPIE() {
   return (
-    <div className='FormDescription'>FormPIE</div>
-  )
+    <>
+      <div className="FormPie">
+        <div className="ConteinerEje">
+          <h2 className="TitlePie">Ejes Transversales</h2>
+          <Form className="FormEje">
+            <p className="SubtitlePie">Seleccione los ejes:</p>
+            <div className="Eje">
+            <Form.Check id={`1`} label={`Sostenibilidad Ambiental`} />
+            <Form.Check id={`2`} label={`Inclusión y Equidad`} />
+            <Form.Check id={`3`} label={`Compromiso Social`} />
+            </div>
+          </Form>
+        </div>
+        <div className="ConteinerPlan">
+          <h2 className="TitlePie">Plan Institucional</h2>
+          <Form className="FormPlan">
+            <p className="SubtitlePie">Seleccione los planes:</p>
+            <div className="ConteinerChecksPlan">
+              <div className="Lie">
+                <Form.Check id={`1`} label={`L.I.E N°1 - Objetivo 1`} />
+                <Form.Check id={`2`} label={`L.I.E N°1 - Objetivo 2`} />
+                <Form.Check id={`3`} label={`L.I.E N°1 - Objetivo 3`} />
+                <Form.Check id={`3`} label={`L.I.E N°1 - Objetivo 4`} />
+              </div>
+              <div className="Lie">
+                <Form.Check id={`1`} label={`L.I.E N°2 - Objetivo 1`} />
+                <Form.Check id={`2`} label={`L.I.E N°2 - Objetivo 2`} />
+                <Form.Check id={`3`} label={`L.I.E N°2 - Objetivo 3`} />
+                <Form.Check id={`3`} label={`L.I.E N°2 - Objetivo 4`} />
+              </div>
+              <div className="Lie">
+                <Form.Check id={`1`} label={`L.I.E N°3 - Objetivo 1`} />
+                <Form.Check id={`2`} label={`L.I.E N°3 - Objetivo 2`} />
+                <Form.Check id={`3`} label={`L.I.E N°3 - Objetivo 3`} />
+                <Form.Check id={`3`} label={`L.I.E N°3 - Objetivo 4`} />
+                <Form.Check id={`3`} label={`L.I.E N°3 - Objetivo 5`} />
+              </div>
+            </div>
+          </Form>
+        </div>
+      </div>
+      <button className="SaveChange">Guardar Cambios</button>
+    </>
+  );
 }
--- src/components/Forms/FormPeriodo.tsx
+++ src/components/Forms/FormPeriodo.tsx
@@ -1,7 +1,87 @@
-import React from 'react'
+import React from "react";
+import DatePicker, { registerLocale } from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { useState } from "react";
+import es from 'date-fns/locale/es'
+registerLocale('es',es)
 
 export default function FormPeriodo() {
+  const [startDate, setDate] = useState(new Date());
+  const [rangeStart, setRangeStart] = useState(new Date());
+  const defaultEndDate = new Date();
+  defaultEndDate.setDate(defaultEndDate.getDate() + 7);
+  const [rangeEnd, setRangeEnd] = useState(defaultEndDate);
+  const today = new Date();
+  const [indexDates, setIndexDates] = useState<string []>([]);
+
+  const selectDateHandler = (d: Date) => {
+    setDate(d);
+    setIndexDates([...indexDates, d.getDate()+ "-"+ (d.getMonth()+1) +"-"+ d.getFullYear().toString()])
+    console.log();
+    
+};
+
+  const selectStartDate = (d: Date) => {
+    setRangeStart(d);
+  };
+
+  const selectEndDate = (d: Date) => {
+    setRangeEnd(d);
+  };
   return (
-    <div className='FormDescription'>FormPeriodo</div>
-  )
+    <div className="FormDescription">
+      <h1>Periodo</h1>
+      <div className="ConteinerBigDate">
+        <div className="ConteinerDate">
+          <h3>Seleccionar Periodo</h3>
+          <div className="ConteinerRange">
+            <div>
+              <p>Desde:</p>
+              <DatePicker
+                locale="es"
+                selectsStart
+                dateFormat="dd/MM/yyyy"
+                selected={rangeStart}
+                minDate={today}
+                startDate={rangeStart}
+                endDate={rangeEnd}
+                onChange={selectStartDate}
+              />
+            </div>
+            <div>
+              <p>Hasta:</p>
+              <DatePicker
+                selectsEnd
+                locale="es"
+                dateFormat="dd/MM/yyyy"
+                selected={rangeEnd}
+                startDate={rangeStart}
+                endDate={rangeEnd}
+                onChange={selectEndDate}
+              />
+            </div>
+          </div>
+        </div>
+        <div className="ConteinerDay">
+          <h3>Seleccion Fechas Puntuales</h3>
+          <DatePicker
+            dateFormat="dd/MM/yyyy"
+            locale="es"
+            minDate={rangeStart}
+            maxDate={rangeEnd}
+            selected={startDate}
+            onChange={selectDateHandler}
+          />
+          <div className="ConteinerDaysSelected">
+            <ul>Fechas Seleccionadas:
+              {indexDates.map((date, index) => (
+                <li key={index}>{date}</li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+      <button className="SaveChange">Guardar Cambios</button>
+    </div>
+  );
 }
