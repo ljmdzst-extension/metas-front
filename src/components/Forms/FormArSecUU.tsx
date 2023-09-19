28331775f91879b70bb58871fedfa35f05b43551
HernanC95 <cavalieri.h95@gmail.com>
2023-09-19 00:15:40 -0300

    fixPeriodo

--- src/components/Forms/FormArSecUU.tsx
+++ src/components/Forms/FormArSecUU.tsx
@@ -18,10 +18,6 @@ interface Relacion {
     nom: string;
   };
 }
-interface Option {
-  value: number;
-  label: string;
-}
 interface listaProgramasSIPPE {
   idProgramaSIPPE: number;
   nom: string;
@@ -32,69 +28,33 @@ export default function FormArSecUU({ onClose }: FormArSecUUrops) {
   const [relaciones, setRelaciones] = useState<Relacion[]>([]);
   const [sippe, setSippe] = useState<listaProgramasSIPPE[]>([]);
   const [relacionSeleccionadas1, setRelacionSeleccionadas1] = useState<
-  Option[]
+  number[]
 >([]);
 const [relacionSeleccionadas2, setRelacionSeleccionadas2] = useState<
-Option[]
+number[]
 >([]);
 const [relacionSeleccionadas3, setRelacionSeleccionadas3] = useState<
-Option[]
+number[]
+>([]);
+const [relacionesSeleccionadas, setRelacionesSeleccionadas] = useState<
+number[]
 >([]);
 const [sippeSeleccionadas, setSippeSeleccionadas] = useState<
 number[]
 >([]);
-useEffect(() => {
-  const fetchData = async () => {
-    try {
-      const response = await axios.get(
-        "http://localhost:4000/metas/v2/bases/"
-      );
-      if (response.data.ok) {
-        const listaRelaciones = response.data.data.listaRelaciones;
-        setRelaciones(listaRelaciones);
-        const listaSIPPE = response.data.data.listaProgramasSIPPE;
-        setSippe(listaSIPPE);
-      } else {
-        console.error("Error en la respuesta de la API");
-      }
-    } catch (error) {
-      console.error("Error al obtener la lista de relaciones:", error);
-    }
-  };
-  fetchData();
-}, []);
   const estadoRelacionesSeleccionadas = useSelector(
     (state: RootState) => state.actividadSlice.listaRelaciones
   );
   const estadoSIPPESeleccionadas = useSelector(
     (state: RootState) => state.actividadSlice.listaProgramasSIPPE
   );
-  const relacionesInternaExtension: Option[] = relaciones
-  .filter((relacion) => relacion.tipoRelacion.nom === "interna_extensión")
-  .map((relacion) => ({
-    value: relacion.idRelacion,
-    label: relacion.nom,
-  }));
-  const relacionesInternaUnl: Option[] = relaciones
-  .filter((relacion) => relacion.tipoRelacion.nom === "interna_unl")
- .map((relacion) => ({
-    value: relacion.idRelacion,
-   label: relacion.nom,
-}));
-  const relacionesUA: Option[] = relaciones
-  .filter((relacion) => relacion.tipoRelacion.nom === "U.A.")
-  .map((relacion) => ({
-    value: relacion.idRelacion,
-    label: relacion.nom,
-  }));
   const sincronizarSelectsRelacion = () => {
     if (estadoRelacionesSeleccionadas) {
-      setRelacionSeleccionadas1(relacionesInternaExtension.filter((relacion)=>estadoRelacionesSeleccionadas.some(valor=>valor===relacion.value)))
-      console.log(relacionSeleccionadas1);
-      setRelacionSeleccionadas2(relacionesInternaUnl.filter((relacion)=>estadoRelacionesSeleccionadas.some(valor=>valor===relacion.value)))
-      setRelacionSeleccionadas3(relacionesUA.filter((relacion)=>estadoRelacionesSeleccionadas.some(valor=>valor===relacion.value)))
-  }}
-
+      setRelacionSeleccionadas1(estadoRelacionesSeleccionadas);
+      setRelacionSeleccionadas2(estadoRelacionesSeleccionadas);
+      setRelacionSeleccionadas3(estadoRelacionesSeleccionadas);
+    }
+  };
   const sincronizarSelectsSIPPE = () => {
     if (estadoSIPPESeleccionadas) {
       setSippeSeleccionadas(estadoSIPPESeleccionadas);
@@ -164,22 +124,24 @@ useEffect(() => {
     onClose();
   };
   const handleRelacionChange1 = (selectedOptions: any) => {
-    setRelacionSeleccionadas1(selectedOptions);
+    const selectedValues = selectedOptions.map((option: any) => option.value);
+    setRelacionSeleccionadas1(selectedValues);
   };
-    const handleRelacionChange2 = (selectedOptions: any) => {
-      setRelacionSeleccionadas2(selectedOptions);
-    };
-    const handleRelacionChange3 = (selectedOptions: any) => {
-      setRelacionSeleccionadas3(selectedOptions);
+   const handleRelacionChange2 = (selectedOptions: any) => {
+     const selectedValues = selectedOptions.map((option: any) => option.value);
+     setRelacionSeleccionadas2(selectedValues);
+   };
+   const handleRelacionChange3 = (selectedOptions: any) => {
+     const selectedValues = selectedOptions.map((option: any) => option.value);
+     setRelacionSeleccionadas3(selectedValues);
    };
   const handleSippeChange = (selectedOptions: any) => {
-     const selectedValues = selectedOptions.map((option: any) => option.value);
-     setSippeSeleccionadas(selectedValues);
-   };
+    const selectedValues = selectedOptions.map((option: any) => option.value);
+    setSippeSeleccionadas(selectedValues);
+  };
   return (
     <>
-     {relaciones && (
-    <div className="FormArSecuu">
+      <div className="FormArSecuu">
         <div className="Areas">
           <h2>Areas Internas de la secretarias</h2>
           <div className="SelectArea">
@@ -190,12 +152,12 @@ useEffect(() => {
               isMulti
               options={relacionesInternaExtension}
               placeholder={"seleccionar"}
-              value={relacionSeleccionadas1}
+              value={relacionesInternaExtension.filter(option => relacionSeleccionadas1.includes(option.value))}
               onChange={handleRelacionChange1}
             />
           </div>
         </div>
-         <div className="Secretarias">
+        <div className="Secretarias">
           <h2>Secretarias</h2>
           <div className="SelectSec">
             <p className="parrafo">Seleccione segun corresponda:</p>
@@ -205,11 +167,11 @@ useEffect(() => {
               isMulti
               options={relacionesInternaUnl}
               placeholder={"seleccionar"}
-              value={relacionSeleccionadas2}
+              value={relacionesInternaUnl.filter(option => relacionSeleccionadas2.includes(option.value))}
               onChange={handleRelacionChange2}
             />
           </div>
-        </div> 
+        </div>
         <div className="UUAA">
           <h2>Unidades Academicas involucradas</h2>
           <div className="SelectUUAA">
@@ -220,7 +182,7 @@ useEffect(() => {
               isMulti
               options={relacionesUA}
               placeholder={"seleccionar"}
-              value={relacionSeleccionadas3}
+              value={relacionesUA.filter(option => relacionSeleccionadas3.includes(option.value))}
               onChange={handleRelacionChange3}
             />
           </div>
@@ -239,9 +201,8 @@ useEffect(() => {
               onChange={handleSippeChange}
             />
           </div>
-        </div> 
-      </div> )}
-      
+        </div>
+      </div>
       <Button
         variant="success"
         className="SaveChange"
