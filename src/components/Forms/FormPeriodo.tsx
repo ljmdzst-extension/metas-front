131e14bc975c4ff400e1da406a9c8bcbb2db4921
Carlos Jonatan Bordón <68975079+YonaBordon@users.noreply.github.com>
2023-11-07 12:20:08 -0300

    validaciones form periodo

--- src/components/Forms/FormPeriodo.tsx
+++ src/components/Forms/FormPeriodo.tsx
@@ -7,7 +7,7 @@ import ListGroup from "react-bootstrap/ListGroup";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../../redux/store";
 import { guardarActividad } from "../../redux/actions/putActividad";
-import { Col, Container, Row } from "react-bootstrap"
+import { Col, Row } from "react-bootstrap"
 import HighlightOffIcon from '@mui/icons-material/HighlightOff';
 
 registerLocale("es", es);
@@ -29,16 +29,17 @@ export default function FormPeriodo({ onClose }: FormPeriodoProps) {
   const [fechaHasta, setFechaHasta] = useState<string | null>(
     estadoActualizado.fechaHasta ?? null
   );
+	const [erroresRango, setErroresRango] = useState<string>('');
   const [listaFechasPuntuales, setListaFechasPuntuales] = useState<
     { idFecha: number | null; fecha: string | null }[]
   >(estadoActualizado.listaFechasPuntuales ?? []);
   const [startDate, setDate] = useState<Date>(new Date());
   const [rangeStart, setRangeStart] = useState<Date>(
-    new Date(estadoActualizado.fechaDesde?.split("-").join("/") || "2023/01/01")
+    new Date(estadoActualizado.fechaDesde?.split("-").join("/") ?? "2023/01/01")
   );
 
   const [rangeEnd, setRangeEnd] = useState<Date>(
-    new Date(estadoActualizado.fechaHasta?.split("-").join("/") || "2023/12/31")
+    new Date(estadoActualizado.fechaHasta?.split("-").join("/") ?? "2023/12/31")
   );
 
 
@@ -62,6 +63,7 @@ export default function FormPeriodo({ onClose }: FormPeriodoProps) {
   };
 
   const selectDateHandler = (d: Date) => {
+		console.log('selectDateHandler', d);
     const dateString = dateToString(d);
 
     if (!indexDates.find((date) => date.fecha === dateString)) {
@@ -70,7 +72,19 @@ export default function FormPeriodo({ onClose }: FormPeriodoProps) {
         idFecha: 0,
         fecha: dateString,
       };
-      setListaFechasPuntuales([...listaFechasPuntuales, nuevaFecha]);
+
+			const listaOrdenada = [...listaFechasPuntuales, nuevaFecha].sort((a, b) => {
+				if (a.fecha && b.fecha) {
+					const dateA = new Date(a.fecha);
+					const dateB = new Date(b.fecha);
+					console.log(dateA.getTime() - dateB.getTime());
+					return dateA.getTime() - dateB.getTime();
+				}
+				return 0;
+			}
+			);
+      
+			setListaFechasPuntuales(listaOrdenada);
     }
   };
 
@@ -91,6 +105,34 @@ export default function FormPeriodo({ onClose }: FormPeriodoProps) {
     setListaFechasPuntuales(filteredDates);
   };
 
+	const highlightSelectedDates = () => {
+		console.log('highlightSelectedDates');
+		const highlightDates: Date[] = [];
+		indexDates.forEach((date) => {
+			if (date.fecha) {
+				const dateToHighlight = new Date(date.fecha + 'T00:00:00-03:00');
+				dateToHighlight.setDate(dateToHighlight.getDate());
+				highlightDates.push(dateToHighlight);
+			}
+		});
+		console.log(indexDates);
+		console.log(highlightDates);
+		return highlightDates;
+	};
+
+	
+	useEffect(() => {
+		const validarRango = () => {
+			if (rangeStart?.getTime() > rangeEnd?.getTime()) {
+				setErroresRango('La fecha de inicio no puede ser mayor a la fecha de fin');
+				return false;
+			}
+			setErroresRango('');
+			return true;
+		}
+		validarRango();
+	}, [rangeStart, rangeEnd]);
+
 	return (
 		<div className=' contenedor-forms mx-3 '>
 			<Row className=' justify-content-around'>
@@ -110,7 +152,7 @@ export default function FormPeriodo({ onClose }: FormPeriodoProps) {
 								dateFormat='dd/MM/yyyy'
 								selected={rangeStart}
 								minDate={new Date('2023/01/01')}
-								maxDate={new Date(indexDates[0]?.fecha?.split('-').join('/') || '2080-01-01')}
+								maxDate={new Date(indexDates[0]?.fecha?.split('-').join('/') ?? '2080-01-01')}
 								startDate={rangeStart}
 								endDate={rangeEnd}
 								onChange={selectStartDate}
@@ -127,7 +169,7 @@ export default function FormPeriodo({ onClose }: FormPeriodoProps) {
 								endDate={rangeEnd}
 								minDate={
 									new Date(
-										indexDates[indexDates.length - 1]?.fecha?.split('-').join('/') || '2023/01/02',
+										indexDates[indexDates.length - 1]?.fecha?.split('-').join('/') ?? '2023/01/02',
 									)
 								}
 								onChange={selectEndDate}
@@ -135,19 +177,20 @@ export default function FormPeriodo({ onClose }: FormPeriodoProps) {
 						</div>
 					</div>
 					<div>
-						<p
-							className={`text-center m-2 ${
-								fechaDesde && fechaHasta
-									? 'texto-fechas-seleccionadas'
-									: 'texto-fechas-no-seleccionadas'
-							}`}
-						>
-							{fechaDesde && fechaHasta
-								? `El rango seleccionado es desde ${printDMA(fechaDesde)} hasta ${printDMA(
-										fechaHasta,
-								  )}`
-								: 'Seleccione un rango de fechas'}
-						</p>
+						{erroresRango && (
+						
+								<p className="texto-fechas-error">{erroresRango}</p>
+							
+						)}
+						{!erroresRango &&
+							(rangeStart && rangeEnd ? (
+								<p className="texto-fechas-seleccionadas">
+									El rango seleccionado es desde {printDMA(fechaDesde ?? '')} hasta{' '}
+									{printDMA(fechaHasta ?? '')}
+								</p>
+							) : (
+								<p className="texto-fechas-no-seleccionadas">Seleccione un rango de fechas</p>
+							))}
 					</div>
 				</Col>
 				<Col className=' border rounded border-black  ' xs={5}>
@@ -157,19 +200,19 @@ export default function FormPeriodo({ onClose }: FormPeriodoProps) {
 						el intervalo de meses seleccionado)
 					</p>
 					<div>
-						<p>Seleccione una Fecha:</p>
 						<DatePicker
 							dateFormat='dd/MM/yyyy'
 							locale='es'
 							minDate={rangeStart}
 							maxDate={rangeEnd}
-							selected={startDate}
+							placeholderText='Seleccione una fecha'
 							onChange={selectDateHandler}
+							highlightDates={highlightSelectedDates()}
 						/>
 					</div>
-					<div className='ConteinerDaysSelected'>
-						<div className='ConteinerFechas'>
-							<span>Fechas Seleccionadas:</span>
+					<div className='ConteinerDaysSelected m-2 ms-0'>
+						<span>Fechas Seleccionadas:</span>
+						<div className='ConteinerFechas' style={{ maxHeight: '200px', overflowY: 'auto' }}>
 							{indexDates.map((date, index) => (
 								<ListGroup.Item
 									key={index}
