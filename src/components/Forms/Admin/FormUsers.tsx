0dca4fc06539b28ad1490605e0a969a7eeb48e4c
Carlos Jonatan Bordón <68975079+YonaBordon@users.noreply.github.com>
2024-07-29 12:27:34 -0300

    tabs

--- src/components/Forms/Admin/FormUsers.tsx
+++ src/components/Forms/Admin/FormUsers.tsx
@@ -1,6 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import { useForm, Controller } from 'react-hook-form';
-import { Form, Button, Row, Col, InputGroup } from 'react-bootstrap';
+import {
+	Form,
+	Button,
+	Row,
+	Col,
+	InputGroup,
+	CloseButton,
+	Tabs,
+	Tab,
+	Container,
+} from 'react-bootstrap';
 import Select, { MultiValue, SingleValue } from 'react-select';
 import IconButton from '@mui/material/IconButton';
 import Visibility from '@mui/icons-material/Visibility';
@@ -11,7 +21,7 @@ import FormInput from '@/components/Common/FormInput';
 import { UserData, UserFormData } from '@/types/UserProps';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../../redux/store';
-import { Add, Close } from '@mui/icons-material';
+import { Add } from '@mui/icons-material';
 
 const MySwal = withReactContent(Swal);
 
@@ -139,131 +149,160 @@ const FormUsers: React.FC<FormUsersProps> = ({ userData, onClose }) => {
 	return (
 		<Form onSubmit={handleSubmit(onSubmit)} className='d-flex flex-column h-100'>
 			<div className='d-flex justify-content-end'>
-				<Close onClick={onClose} />
+				<CloseButton onClick={onClose} />
 			</div>
-			<Row>
-				<Col>
-					<FormInput control={control} name='nom' label='Nombre' rules={validationRules.nom} />
-				</Col>
-				<Col>
-					<FormInput control={control} name='ape' label='Apellido' rules={validationRules.ape} />
-				</Col>
-			</Row>
-			<Row>
-				<Col>
-					<FormInput
-						control={control}
-						name='email'
-						label='Email'
-						type='email'
-						rules={validationRules.email}
-					/>
-				</Col>
-				<Col>
-					<Form.Group controlId='formPass' className='mb-2'>
-						<Form.Label>Contraseña</Form.Label>
-						<div className='d-flex align-items-center'>
-							<Controller
-								name='pass'
-								control={control}
-								rules={validationRules.pass}
-								render={({ field, fieldState: { error } }) => (
-									<div>
-										<Form.Control
-											size='sm'
-											type={showPassword ? 'text' : 'password'}
-											isInvalid={!!error}
-											{...field}
+			<Tabs variant='tabs' transition className=' mb-2' fill>
+				<Tab eventKey='user' title='Usuario'>
+					<Container>
+						<Row>
+							<Col>
+								<FormInput
+									control={control}
+									name='nom'
+									label='Nombre'
+									rules={validationRules.nom}
+								/>
+							</Col>
+							<Col>
+								<FormInput
+									control={control}
+									name='ape'
+									label='Apellido'
+									rules={validationRules.ape}
+								/>
+							</Col>
+						</Row>
+						<Row>
+							<Col>
+								<FormInput
+									control={control}
+									name='email'
+									label='Email'
+									type='email'
+									rules={validationRules.email}
+								/>
+							</Col>
+							<Col>
+								<Form.Group controlId='formPass' className='mb-2'>
+									<Form.Label>Contraseña</Form.Label>
+									<div className='d-flex align-items-center'>
+										<Controller
+											name='pass'
+											control={control}
+											rules={validationRules.pass}
+											render={({ field, fieldState: { error } }) => (
+												<div>
+													<Form.Control
+														size='sm'
+														type={showPassword ? 'text' : 'password'}
+														isInvalid={!!error}
+														{...field}
+													/>
+													<Form.Control.Feedback type='invalid'>
+														{error?.message}
+													</Form.Control.Feedback>
+												</div>
+											)}
 										/>
-										<Form.Control.Feedback type='invalid'>{error?.message}</Form.Control.Feedback>
+										<IconButton onClick={toggleShowPassword} className='ml-2'>
+											{showPassword ? <VisibilityOff /> : <Visibility />}
+										</IconButton>
 									</div>
-								)}
-							/>
-							<IconButton onClick={toggleShowPassword} className='ml-2'>
-								{showPassword ? <VisibilityOff /> : <Visibility />}
-							</IconButton>
-						</div>
-					</Form.Group>
-				</Col>
-			</Row>
-			<Form.Group controlId='formYear' className='mb-2'>
-				<Form.Label>Año</Form.Label>
-				{loadingYears ? (
-					<p>Cargando años...</p>
-				) : (
-					<Controller
-						name='anio'
-						control={control}
-						render={({ field }) => (
-							<Select
-								{...field}
-								options={yearOptions}
-								onChange={(selectedOption: SingleValue<OptionProps>) => {
-									field.onChange(selectedOption?.value || null);
-									setSelectedYear(selectedOption);
-								}}
-								value={yearOptions.find((option) => option.value === field.value) || null}
-							/>
-						)}
-					/>
-				)}
-			</Form.Group>
-			<Form.Group controlId='formPrograms' className='mb-2'>
-				<Form.Label>Programas</Form.Label>
-				{selectedYear && !loadingPrograms ? (
-					<InputGroup className='d-flex'>
-						<Controller
-							name='programas'
-							control={control}
-							render={({ field }) => (
-								<Select
-									options={userProgramsOptions}
-									onChange={(selectedOption: SingleValue<OptionProps>) => {
-										const value = selectedOption?.value || 0;
-										field.onChange([value]);
-										handleProgramChange(selectedOption);
-									}}
-									isClearable
-									placeholder='Agregar Programa'
-									className='flex-grow-1'
+								</Form.Group>
+							</Col>
+						</Row>
+					</Container>
+				</Tab>
+				<Tab eventKey='metas' title='Planificaciones y resultados'>
+					<Container>
+						<Form.Group controlId='formYear' className='mb-2'>
+							<Form.Label>Año</Form.Label>
+							{loadingYears ? (
+								<p>Cargando años...</p>
+							) : (
+								<Controller
+									name='anio'
+									control={control}
+									render={({ field }) => (
+										<Select
+											{...field}
+											options={yearOptions}
+											onChange={(selectedOption: SingleValue<OptionProps>) => {
+												field.onChange(selectedOption?.value || null);
+												setSelectedYear(selectedOption);
+											}}
+											value={yearOptions.find((option) => option.value === field.value) || null}
+										/>
+									)}
 								/>
 							)}
-						/>
-						<IconButton className='ml-2' onClick={() => console.log('Agregar nuevo programa')}>
-							<Add />
-						</IconButton>
-					</InputGroup>
-				) : (
-					<p>Seleccione un año primero</p>
-				)}
-			</Form.Group>
-			<Form.Group controlId='formAreas' className='mb-2'>
-				<Form.Label>Áreas</Form.Label>
-				{selectedProgram && !loadingAreas ? (
-					<InputGroup className='d-flex'>
-						<Controller
-							name='areas'
-							control={control}
-							render={({ field }) => (
-								<Select
-									options={allAreasOptions}
-									onChange={(selectedOption: MultiValue<OptionProps>) => {
-										const value = selectedOption.map((opt) => opt.value);
-										field.onChange(value);
-									}}
-									isClearable
-									isMulti
-									placeholder='Agregar Área'
-									className='flex-grow-1'
-									value={userAreasOptions}
-								/>
+						</Form.Group>
+						<Form.Group controlId='formPrograms' className='mb-2'>
+							<Form.Label>Programas</Form.Label>
+							{selectedYear && !loadingPrograms ? (
+								<InputGroup className='d-flex'>
+									<Controller
+										name='programas'
+										control={control}
+										render={({ field }) => (
+											<Select
+												options={userProgramsOptions}
+												onChange={(selectedOption: SingleValue<OptionProps>) => {
+													const value = selectedOption?.value || 0;
+													field.onChange([value]);
+													handleProgramChange(selectedOption);
+												}}
+												isClearable
+												placeholder='Agregar Programa'
+												className='flex-grow-1'
+											/>
+										)}
+									/>
+									<IconButton
+										className='ml-2'
+										onClick={() => console.log('Agregar nuevo programa')}
+									>
+										<Add />
+									</IconButton>
+								</InputGroup>
+							) : (
+								<p>Seleccione un año primero</p>
 							)}
-						/>
-					</InputGroup>
-				) : (
-					<p>Seleccione un programa primero</p>
-				)}
-			</Form.Group>
+						</Form.Group>
+						<Form.Group controlId='formAreas' className='mb-2'>
+							<Form.Label>Áreas</Form.Label>
+							{selectedProgram && !loadingAreas ? (
+								<InputGroup className='d-flex'>
+									<Controller
+										name='areas'
+										control={control}
+										render={({ field }) => (
+											<Select
+												options={allAreasOptions}
+												onChange={(selectedOption: MultiValue<OptionProps>) => {
+													const value = selectedOption.map((opt) => opt.value);
+													field.onChange(value);
+												}}
+												isClearable
+												isMulti
+												placeholder='Agregar Área'
+												className='flex-grow-1'
+												value={userAreasOptions}
+											/>
+										)}
+									/>
+								</InputGroup>
+							) : (
+								<p>Seleccione un programa primero</p>
+							)}
+						</Form.Group>
+					</Container>
+				</Tab>
+				<Tab eventKey='gestor' title='Gestor'>
+					Programas
+				</Tab>
+			</Tabs>
+
 			<Button type='submit' variant='primary' className='mt-auto'>
 				Guardar
 			</Button>
